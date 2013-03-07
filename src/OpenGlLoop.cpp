4cfbfebf1ea7e2e4d2a622bbfa36a28523d72dd8
Marton Suranyi <marton.suranyi@gmail.com>
2013-03-08 00:43:58 +0100

    some cleanup

--- src/OpenGlLoop.cpp
+++ src/OpenGlLoop.cpp
@@ -18,44 +18,9 @@ namespace cw
   namespace graph
   {
 
-void startGlFw()
-{
-  if ( !glfwInit() )
-  {
-    throw GlException( "Failed to initialize GLFW!" );
-  }
-  LOG(DEBUG) << "GLFW initialized.";
+void startGlFw();
 
-  glfwOpenWindowHint( GLFW_FSAA_SAMPLES, 4 ); // 4x anit-aliasing
-  glfwOpenWindowHint( GLFW_OPENGL_VERSION_MAJOR, 2 );
-  glfwOpenWindowHint( GLFW_OPENGL_VERSION_MINOR, 1 );
-
-  const int COLOR_DEPTH = 32;
-  if ( !glfwOpenWindow( SCREEN_X, SCREEN_Y, 0,0,0,0, COLOR_DEPTH, 0, GLFW_WINDOW ) )
-  {
-    glfwTerminate();
-    throw GlException( "Failed to open GLFW window!" );
-  }
-  glfwSetWindowTitle( "CelluloseWar" );
-  int actualX;
-  int actualY;
-  glfwGetWindowSize( &actualX, &actualY );
-  LOG(DEBUG) << "GLFW Window opened. " <<
-    "requested_size=" << SCREEN_X << "x" << SCREEN_Y << " " <<
-    "actual_size=" << actualX << "x" << actualY;
-
-}
-
-void initGlew()
-{
-  glewExperimental = true;
-
-  if ( glewInit() != GLEW_OK)
-  {
-    throw GlException( "Failed to initialize GLEW!" );
-  }
-  LOG(DEBUG) << "GLEW initialized successfully.";
-}
+void initGlew();
 
 OpenGlLoop::OpenGlLoop()
 {}
@@ -96,6 +61,45 @@ void OpenGlLoop::run()
       && glfwGetWindowParam( GLFW_OPENED ) );
 
   GlfwCallbackRepo::terminate();
+}
+
+void startGlFw()
+{
+  if ( !glfwInit() )
+  {
+    throw GlException( "Failed to initialize GLFW!" );
+  }
+  LOG(DEBUG) << "GLFW initialized.";
+
+  glfwOpenWindowHint( GLFW_FSAA_SAMPLES, 4 ); // 4x anit-aliasing
+  glfwOpenWindowHint( GLFW_OPENGL_VERSION_MAJOR, 2 );
+  glfwOpenWindowHint( GLFW_OPENGL_VERSION_MINOR, 1 );
+
+  const int COLOR_DEPTH = 32;
+  if ( !glfwOpenWindow( SCREEN_X, SCREEN_Y, 0,0,0,0, COLOR_DEPTH, 0, GLFW_WINDOW ) )
+  {
+    glfwTerminate();
+    throw GlException( "Failed to open GLFW window!" );
+  }
+  glfwSetWindowTitle( "CelluloseWar" );
+  int actualX;
+  int actualY;
+  glfwGetWindowSize( &actualX, &actualY );
+  LOG(DEBUG) << "GLFW Window opened. " <<
+    "requested_size=" << SCREEN_X << "x" << SCREEN_Y << " " <<
+    "actual_size=" << actualX << "x" << actualY;
+
+}
+
+void initGlew()
+{
+  glewExperimental = true;
+
+  if ( glewInit() != GLEW_OK)
+  {
+    throw GlException( "Failed to initialize GLEW!" );
+  }
+  LOG(DEBUG) << "GLEW initialized successfully.";
 }
 
   }
