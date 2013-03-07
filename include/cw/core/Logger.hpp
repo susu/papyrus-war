fcc5cbec4db55f5029fec7affae6700deef16e5c
Marton Suranyi <marton.suranyi@gmail.com>
2013-03-08 00:56:15 +0100

    dont logging in release config

--- include/cw/core/Logger.hpp
+++ include/cw/core/Logger.hpp
@@ -2,9 +2,18 @@
 #define CW_CORE_LOGGER_HPP_INC
 
 #include <iostream>
+#include <fstream>
+
+#ifdef CW_DEBUG
 
 #define LOG_MSG(loglevel) \
   logger.getGuardedStream( (loglevel), __FILE__, __LINE__, __PRETTY_FUNCTION__ )
+
+#else // CW_DEBUG
+
+#define LOG_MSG(a) logger.getNullStream()
+
+#endif
 
 #define LOG(level) LOG_MSG(cw::core::Logger::level)
 
@@ -56,11 +65,16 @@ namespace cw
           return std::cout;
         }
 
+        std::ostream& getNullStream()
+        {
+          return m_null;
+        }
+
         void endLog();
       private:
         void startLog( LogLevel loglevel, const char * file, int line,
                        const char * pretty);
-
+        std::ofstream m_null;
     };
   }
 }
