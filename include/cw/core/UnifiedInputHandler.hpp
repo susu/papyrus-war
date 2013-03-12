11ace4926472e0cbe4c71a92b6042ec5cc16212e
Marton Suranyi <marton.suranyi@gmail.com>
2013-03-13 00:57:38 +0100

    unified-input handler interface added -- Unified input means device-independent

---
+++ include/cw/core/UnifiedInputHandler.hpp
@@ -0,0 +1,33 @@
+#ifndef CW_CORE_UNIFIED_INPUT_HANDLER_HPP_INC
+#define CW_CORE_UNIFIED_INPUT_HANDLER_HPP_INC
+
+namespace cw
+{
+  namespace core
+  {
+    class UnifiedInputHandler
+    {
+      public:
+        enum class ZoomDir
+        {
+          IN,
+          OUT
+        };
+        enum class ScrollDir
+        {
+          RIGHT,
+          DOWN,
+          LEFT,
+          UP
+        };
+        virtual ~UnifiedInputHandler()
+        {}
+
+        virtual void clickedAt(int x, int y) = 0;
+        virtual void zoom(ZoomDir direction) = 0;
+        virtual void startScroll(ScrollDir direction) = 0;
+        virtual void stopScroll() = 0;
+    };
+  }
+}
+#endif
