faf311b4e955a341f11206b0cc21d9d3ed01eec4
Marton Suranyi <marton.suranyi@gmail.com>
2013-04-11 22:24:48 +0200

    PickingInterface added

---
+++ include/cw/core/PickingInterface.hpp
@@ -0,0 +1,15 @@
+#ifndef CW_CORE_PICKING_INTERFACE_HPP_INC
+#define CW_CORE_PICKING_INTERFACE_HPP_INC
+
+namespace cw
+{
+  namespace core
+  {
+    class PickingInterface
+    {
+      public:
+    };
+  }
+}
+
+#endif
