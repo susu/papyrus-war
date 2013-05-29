4e34ca118be567316b2a433d502d275d396b9603
Marton Suranyi <marton.suranyi@gmail.com>
2013-05-29 21:22:46 +0200

    enum class can be explicitly casted to underlying_type

--- include/cw/core/Types.hpp
+++ include/cw/core/Types.hpp
@@ -2,6 +2,7 @@
 #define CW_CORE_TYPES_HPP_INC
 
 #include <memory>
+#include <type_traits>
 
 template<class T>
 using Ref = std::shared_ptr<T>;
@@ -13,6 +14,12 @@ namespace cw
     class PaperBoat;
     typedef Ref<PaperBoat> PaperBoatRef;
   }
+}
+
+template<typename EnumClass>
+auto to_underlying(EnumClass e) -> typename std::underlying_type<EnumClass>::type
+{
+  return static_cast<typename std::underlying_type<EnumClass>::type>(e);
 }
 
 #endif
