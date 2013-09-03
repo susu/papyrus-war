e0296721c1efa3556f27668939ec5e7c16a82be8
Marton Suranyi <marton.suranyi@gmail.com>
2013-09-03 20:08:56 +0200

    add make_unique

--- include/cpp11hacks.hpp
+++ include/cpp11hacks.hpp
@@ -8,4 +8,16 @@
 #define noexcept throw()
 #endif
 
+#include <memory>
+
+// C++14 hacks
+namespace std
+{
+  template<typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args&&>(args)...));
+  }
+}
+
 #endif
